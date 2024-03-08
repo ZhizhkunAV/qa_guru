e78edb4ea6fd5d7343d551422f2535ce8bcc503a
ZhizhkunAV <116073898+ZhizhkunAV@users.noreply.github.com>
2024-03-08 22:21:41 +0400

    Update Main.java

--- src/main/java/av/zizkun/Main.java
+++ src/main/java/av/zizkun/Main.java
@@ -1,7 +1,10 @@
 package av.zizkun;
 
 public class Main {
+    
+    private static String msg = "Hello qa_guru!!!"
+    
     public static void main(String[] args) {
-        System.out.println("Hello qa_guru!!!");
+        System.out.println(msg");
     }
 }
