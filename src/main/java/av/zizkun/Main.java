f152a81052babddb1c92be3818f21e463d1125dd
Andrey_Zhizhkun <zizkun.av@list.ru>
2024-03-09 12:31:26 +0400

    Конфликтq1

--- src/main/java/av/zizkun/Main.java
+++ src/main/java/av/zizkun/Main.java
@@ -2,7 +2,7 @@ package av.zizkun;
 
 public class Main {
 
-    private static String msg = "Hello qa.guru!!!!";
+    private static String msg = "Hello qa.guruujdyj!!!!";
 
     public static void main(String[] args) {
         System.out.println(getMessage());
